961fb1735106aa8fca52c50a4714d7d008848c91
calexandar <alexandercvet243@gmail.com>
2025-05-28 21:15:29 +0200

    aded payment model

---
+++ modules/Payment/Payment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Payment;
+
+use App\Models\User;
+use Modules\Order\Models\Order;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Payment extends Model
+{
+    /** @use HasFactory<\Database\Factories\PaymentFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
