c6bca978c3cdd0df6aaa5ad08597f1148e453c57
calexandar <alexandercvet243@gmail.com>
2025-06-12 22:16:34 +0200

    aded user dto

--- modules/Order/Http/Controllers/CheckoutController.php
+++ modules/Order/Http/Controllers/CheckoutController.php
@@ -5,6 +5,7 @@ namespace Modules\Order\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Validation\ValidationException;
 use Modules\Order\Actions\PurchaseItems;
+use Modules\Order\DTOs\PendingPayment;
 use Modules\Order\Exceptions\PaymentFailedException;
 use Modules\Order\Http\Requests\CheckoutRequest;
 use Modules\Payment\PayBuddy;
@@ -19,9 +20,11 @@ class CheckoutController extends Controller
     public function __invoke(CheckoutRequest $request)
     {
         $cartItems = CartItemCollection::fromCheckoutData($request->input('products'));
-
         $payBuddy = new PayBuddy;
         $payBuddy->make();
+
+        $pendingPayment = new PendingPayment($payBuddy, $request->input('payment_token'));
+        $userDto = UserDto::fromEloquentModel
 
         try {
             $order = $this->purchaseItems->handle(
