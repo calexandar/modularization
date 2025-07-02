b03520a0cf0a808ddcfd7cb20f358b1b781d13fc
calexandar <alexandercvet243@gmail.com>
2025-07-02 21:50:11 +0200

    aded payment notify

---
+++ modules/Payment/PaymentFailed.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Payment;
+
+use Modules\Order\DTOs\OrderDto;
+use Modules\User\UserDto;
+
+readonly class PaymentFailed
+{
+    public function __construct(
+        public OrderDto $order,
+        public UserDto $user,
+        public string $reason,
+    ) {
+    }
+}
