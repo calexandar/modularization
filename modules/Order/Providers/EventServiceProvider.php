75de3db787b36fc9fca35b1b5aa6abe57a8d8b33
calexandar <alexandercvet243@gmail.com>
2025-07-05 22:02:29 +0200

    aded payorder

--- modules/Order/Providers/EventServiceProvider.php
+++ modules/Order/Providers/EventServiceProvider.php
@@ -5,12 +5,14 @@ namespace Modules\Order\Providers;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as BaseEventServiceProvider;
 use Modules\Order\Events\OrderFullfiled;
 use Modules\Order\Events\SendOrderConfirmationEmail;
+use Modules\Payment\PayOrder;
 
 class EventServiceProvider extends BaseEventServiceProvider
 {
     protected $listen = [
         OrderFullfiled::class => [
             SendOrderConfirmationEmail::class,
+            PayOrder::class
         ],
     ];
 }
